97f60c151de6281284be22bfb2200ded44c47708
Charlotte Campbell <charlottecampbell.193@gmail.com>
2022-03-03 10:56:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,9 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+
+  source  = "app.terraform.io/charlotte-campbell/s3-bucket/aws"
+  version = "2.8.0"
 
   bucket = "my-s3-bucket"
   bucket_prefix = charlottecampbell
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
 
 }
